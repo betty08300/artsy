c76122dbcd0709459b899cd2748a9097cde31e4f
Betty Wren <wreny641@gmail.com>
2019-07-30 17:23:00 -0400

    complete session_api_util

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,35 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    helper_method :current_user, :current_user_id, :logged_in?
+
+    def current_user
+        return nil unless session[:session_token]
+        @current_user ||= User.find_by_session_token(session[:session_token])
+    end 
+
+    def current_user_id
+        current_user ? current_user.id : nil
+    end 
+
+    def logged_in?
+        !!current_user
+    end 
+
+    def login(user)
+        session[:session_token] = user.reset_token!
+    end 
+
+    def logout 
+        current_user.reset_token!
+        session[:session_token] = nil 
+        @current_user = nil
+    end 
+
+    def require_login
+        if !logged_in?
+            render json: {base: ['invalid credentials']}, status: 401
+        end 
+    end 
+
 end
---
+++ app/models/user.rb
@@ -0,0 +1,55 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  first_name      :string           not null
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+    validates :email, :session_token, uniqueness: true 
+    validates :email, :password_digest, :session_token, presence: true 
+    validates :password, length: {minimum:6, allow_nil: true}
+
+    # need assoication 
+
+    after_initialize :ensure_session_token
+    attr_reader :password
+
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        return nil unless user && user.is_password?(password)
+        user 
+    end 
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end 
+
+    def is_password?(password)
+        BCrypt::Password.new(password_digest).is_password?(password)
+    end 
+
+    def self.generate_session_token
+        SecureRandom.urlsafe_base64(10)
+    end 
+
+    def reset_token!
+        self.session_token = User.generate_session_token
+        self.save!
+        self.session_token
+    end 
+
+    def ensure_session_token
+        self.session_token ||=User.generate_session_token
+    end 
+
+end
+
