0f50b2be50a40c2edec927ef5500358b37accf38
Betty Wren <wreny641@gmail.com>
2019-07-30 21:49:18 -0400

    add user and session routes

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -9,7 +9,7 @@ class Api::SessionsController < ApplicationController
         end 
     end 
 
-    def destory 
+    def destroy 
         @user = current_user
         if @user 
             logout 
