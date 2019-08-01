93b7e41887b23c15a307094aabae941261a930e5
Betty Wren <wreny641@gmail.com>
2019-08-01 00:36:46 -0400

    fix seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-
-email: 'betty2019',
-password: 'betty2019'
